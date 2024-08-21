259e831a56fab7132127ce1792643b778222a007
paul <ophantomo990@gmail.com>
2024-08-21 10:27:47 -0700

    static collected

--- flashy/settings.py
+++ flashy/settings.py
@@ -4,7 +4,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -28,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'account.apps.AccountConfig',
-    "whitenoise.runserver_nostatic",
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
